e66aacc4c44926d84eaee20bdc886b571ff1ea70
KanapongAiamtip <kannaphongi62@nu.ac.th>
2021-06-29 02:26:29 +0700

    C1 Hello Dart

---
+++ C1 Hello Dart/Reverseonly3.dart
@@ -0,0 +1,10 @@
+import 'dart:io';
+
+void main() {
+  String? input = stdin.readLineSync();
+  String? input2 = stdin.readLineSync();
+  String? input3 = stdin.readLineSync();
+  print(input3);
+  print(input2);
+  print(input);
+}
